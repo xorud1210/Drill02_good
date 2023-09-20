cc6f1cd179e725ab94c7a423571a129d1feb73fe
xorud1210 <143979587+xorud1210@users.noreply.github.com>
2023-09-20 18:13:28 +0900

    화면 그리기 함수 완성

--- character_runs.py
+++ character_runs.py
@@ -8,6 +8,12 @@ grass = load_image("grass.png")
 character = load_image('character.png')
 
 
+def render_all(x, y):
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(x, y)
+    delay(0.01)
+
 def run_circle():
     print('CIRLCE')
 
@@ -15,26 +21,23 @@ def run_circle():
     for deg in range(0, 360, 5):
         x = cx + r * math.cos(deg / 360 * 2 * math.pi)
         y = cy + r * math.sin(deg / 360 * 2 * math.pi)
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        delay(0.01)
+        render_all(x, y)
 
         
 def run_rectangle():
     print('RECTANGLE')
 
     # bottom line
-    for x in range(50,750+1,10):
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, 90)
-        delay(0.01)
+    for x in range(50,750+1,5):
+        render_all(x, 90)
+
+    #for x in range(750, 50, -10):
+        
         
 
 
 while True:
-    #run_circle()
+    run_circle()
     run_rectangle()
     break
 
