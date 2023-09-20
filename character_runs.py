f09f02d63db5fcb9a2d27fcd3b20fa2a57f0bd7e
xorud1210 <143979587+xorud1210@users.noreply.github.com>
2023-09-20 18:24:35 +0900

    사각 운동 함수 완성

--- character_runs.py
+++ character_runs.py
@@ -25,19 +25,17 @@ def run_circle():
 
         
 def run_rectangle():
-    print('RECTANGLE')
-
     # bottom line
-    #for x in range(50, 750+1, 5):
-    #    render_all(x, 90)
+    for x in range(50, 750+1, 5):
+        render_all(x, 90)
 
     # right line
-    #for y in range(90,550+1,5):
-    #    render_all(750,y)
+    for y in range(90,550+1,5):
+        render_all(750,y)
 
     # top line
-    #for x in range(750, 50-1, -5):
-    #    render_all(x, 550)
+    for x in range(750, 50-1, -5):
+        render_all(x, 550)
 
     # left_line
     for y in range(550,90-1,-5):
