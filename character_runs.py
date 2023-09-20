b1232fc6904d5f15348604c545b586ee6cf99d2b
xorud1210 <143979587+xorud1210@users.noreply.github.com>
2023-09-20 17:57:52 +0900

    원그리기 완성

--- character_runs.py
+++ character_runs.py
@@ -14,7 +14,17 @@ def run_circle():
     grass.draw_now(400, 30)
     character.draw_now(400,90)
     delay(1)
-    pass
+
+    cx, cy, r = 800 / 2, 600 / 2, 200
+    for deg in range(0, 360, 5):
+        x = cx + r * math.cos(deg / 360 * 2 * math.pi)
+        y = cy + r * math.sin(deg / 360 * 2 * math.pi)
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        delay(0.01)
+
+        
 def run_rectangle():
     print('RECTANGLE')
     pass
